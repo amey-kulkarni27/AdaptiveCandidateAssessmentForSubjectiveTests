aeb95ed8222cd57c716294160ccb99ea5417f973
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-10-15 18:25:15 +0530

    scraping

---
+++ Scraping/NCERT/byjus.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup, NavigableString, Tag
+import string
+import re
+import os
+import csv
+
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url_base = 'https://byjus.com/ncert-solutions-class'
+classes = [str(i) for i in range(4, 13)]
+subjects = ['social-science-history', 'social-science-geography', 'science', 'english', 'civics']
+for cls in classes:
+    rel_path = cls
+    if not os.path.exists(rel_path):
+        os.mkdir(rel_path)
+    for subject in subjects:
+        rel_path = cls + '/' + subject
+        if not os.path.exists(rel_path):
+            os.mkdir(rel_path)
+        parent_url = url_base + '-' + cls + '-' + subject
+        page = requests.get(parent_url, headers=headers)
+        if page.status_code == 404:
+            # print("Page " + cls + " " + subject + " Does Not Exist")
+            continue
+        try:
+                soup = BeautifulSoup(page.content, 'html.parser')  
+        except:
+            # print("Page " + cls + " " + subject + " Error")
+            continue
+
+        tables = soup.findChildren('table')
+        if len(tables) == 0:
+            continue
+        my_table = tables[0]
+        rows = my_table.findChildren(['th', 'tr'])
+        c_num = 0
+        for row in rows:
+            c_num += 1
+            content = ""
+            cells = row.findChildren('td')
+            if len(cells) > 1:
+                continue
+            for cell in cells:
+                row_val = cell.string
+                # print(row_val)
+                # row_val = (row_val.translate(str.maketrans('', '', string.punctuation))).lower()
+                # row_val = row_val.translate(str.maketrans('', '', '-'))
+                if row_val == 0:
+                    continue
+                try:
+                    row_val = re.sub(r'[^\w\s]','',row_val).lower()
+                except:
+                    continue
+                # print(row_val)
+                # print('\n\n')
+                row_val = row_val.split()
+                try:
+                    ind = row_val.index("chapter")
+                except:
+                    print(parent_url, chapter_suffix)
+                    continue
+                chapter_suffix = '-'.join(row_val[ind:])
+
+                chapter_url = parent_url + '-' + chapter_suffix
+                chapter_page = requests.get(chapter_url, headers=headers)
+                if chapter_page.status_code == 404:
+                    print("chapter_page " + chapter_suffix + " Does Not Exist")
+                    continue
+                try:
+                        chapter_soup = BeautifulSoup(chapter_page.content, 'html.parser')  
+                except:
+                    print("chapter_page " + chapter_suffix + " Error")
+                    continue
+
+                article_content = chapter_soup.find('article',attrs={"id" : re.compile("post-[d]*")}).findAll(['p', 'h1', 'h2', 'h3', 'h4'])
+                
+                for row in article_content:
+                    content += row.text
+                    content += '\n'
+                    # print(row.text)
+            with open('./{}.txt'.format(rel_path + '/' + str(c_num)), mode='w', encoding='utf-8') as file:
+                file.write(content)
--- Scraping/NCERT/sample.py
+++ Scraping/NCERT/sample.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag
 import re
 import csv
 import time
 import random
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-url = 'https://byjus.com/ncert-solutions-class-8-social-science-history-chapter-3-ruling-the-countryside/'
+# url = 'https://byjus.com/ncert-solutions-class-9-social-science-geography-chapter-4-climate/'
+# url = 'https://byjus.com/ncert-solutions-class-8-social-science-history-chapter-3-ruling-the-countryside/'
+url = 'https://byjus.com/ncert-solutions-class-6-social-science-history-chapter-2-from-hunting-gathering-to-growing-food/'
 page = requests.get(url, headers=headers)
 if page.status_code == 404:
     print("Page Does Not Exist")
@@ -16,5 +18,29 @@ try:
         soup = BeautifulSoup(page.content, 'html.parser')  
 except:
     print("Page Error")
-for row in soup.find('article',attrs={"id" : re.compile("post-[d]*")}).findAll('p'):
+article_content = soup.find('article',attrs={"id" : re.compile("post-[d]*")}).findAll(['p', 'h1', 'h2', 'h3', 'h4'])
+for row in article_content:
     print(row.text)
+    print()
+
+# i = 0
+# for header in soup.find_all('h2'):
+#     nextNode = header
+#     print(1)
+#     while True:
+#         nextNode = nextNode.nextSibling
+#         if nextNode is None:
+#             break
+#         # To check if next sibling is a text node
+#         if isinstance(nextNode, NavigableString) and i == 2:
+#             print (nextNode.strip())
+#             pass
+#         # To check if next sibling is an element
+#         if isinstance(nextNode, Tag):
+#             if nextNode.name == "h2":
+#                 i += 1
+#                 break
+#             if i == 2:
+#                 print (nextNode.get_text(strip=True).strip())
+#                 pass
+# # print(i)
