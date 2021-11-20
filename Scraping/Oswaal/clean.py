3eaa054c91b524950a7d329ddaddb2f94aa94fda
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-11-20 12:39:17 +0530

    8

--- Scraping/Oswaal/clean.py
+++ Scraping/Oswaal/clean.py
@@ -27,7 +27,7 @@ def clean(s):
     s = re.sub(r'\d.\d=\d', "", s)
     return s
 
-folder = "9/"
+folder = "8/"
 for topic in os.listdir(folder):
     for filename in os.listdir(folder + topic + "/"):
         if filename.endswith(".csv"):
