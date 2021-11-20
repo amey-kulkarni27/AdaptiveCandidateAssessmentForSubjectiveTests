106b42fbbeb410bcff8e95fbc6cd8fed155d9707
amey-kulkarni27 <amey.kulkarni@iitgn.ac.in>
2021-11-20 16:08:18 +0530

    to be cleaned

--- Scraping/pdf_scrape.py
+++ Scraping/pdf_scrape.py
@@ -22,15 +22,76 @@ def create_dataframe(qs, ans):
         })
     return df, len(qs)
 
-searchphrases = ["Very Short Answer Type Question", "Short Answer Type Question", "Long Answer Type Question", "Value Based Question", "High Order Thinking Skills (HOTS) Question", "Practical Based Question"]
+searchphrases = ["Very Short Answer", "Short Answer", "DONTUSEShort Answers – II", "Long Answer", "Value Based", "High Order Thinking", "Practical Based"]
 q_search = re.compile("Q\. \d+")
-pg_num1 = re.compile("\[ \d+")
-pg_num2 = re.compile("\d+ \]")
-textfile = open('S_10.txt', 'r')
-lines = textfile.readlines()
-ctr = 0
+
 
 folder = "biology/"
+qctr = 0
+ctr = 1
 for chapter in os.listdir(folder):
-    for filename in os.listdir(folder + chapter + "/"):
-        fpath = folder + chapter + "/" + filename
+    csv_folder = folder + chapter + "/CSVs/"
+    if not os.path.exists(csv_folder):
+        os.mkdir(csv_folder)
+    filenum = 1
+    while True:
+        txt = folder + chapter + "/" + str(filenum) + ".txt"
+        # print(txt)
+        if os.path.exists(txt) == False:
+            break
+        filenum += 1
+        textfile = open(txt, 'r')
+        lines = textfile.readlines()
+        line_num = 0
+        first_line = ""
+        q_list, a_list, qs, ans, q, a = [], [], "", "", False, False
+        for line in lines:
+            line = line.rstrip()
+            # Remove Form Feed character
+            line = line.replace('\x0c', '')
+            line = re.sub(r'\[[^)]*\]', '', line)
+            if line_num == 0:
+                first_line = line
+                line_num += 1
+                continue
+            if q_search.match(line):
+                if(len(q_list)) and a:
+                    a_list.append(ans)
+                q = True
+                a = False
+                qs = line
+            elif line.startswith("Ans."):
+                if q:
+                    q_list.append(qs)
+                q = False
+                a = True
+                ans = line
+            elif q:
+                if (line.isupper() and all(x.isalpha() or x.isspace() or x==":" for x in line)):
+                    continue
+                qs += " " + line
+            elif a:
+                if (line.isupper() and all(x.isalpha() or x.isspace() or x==":" for x in line)):
+                    continue
+                ans += " " + line
+        if ans != "":
+            a_list.append(ans)
+        df, n_q = create_dataframe(q_list, a_list)
+        qctr += n_q
+
+        if first_line.startswith(searchphrases[0]):
+            df.to_csv(csv_folder + "vs-" + str(ctr) + ".csv")
+        elif first_line.startswith(searchphrases[1]):
+            df.to_csv(csv_folder + "s-" + str(ctr) + ".csv")
+        elif first_line.startswith(searchphrases[3]):
+            df.to_csv(csv_folder + "l-" + str(ctr) + ".csv")
+        elif first_line.startswith(searchphrases[4]):
+            df.to_csv(csv_folder + "val-" + str(ctr) + ".csv")
+        elif first_line.startswith(searchphrases[5]):
+            df.to_csv(csv_folder + "hots-" + str(ctr) + ".csv")
+        elif first_line.startswith(searchphrases[6]):
+            df.to_csv(csv_folder + "pb-" + str(ctr) + ".csv")
+        else:
+            print(first_line)
+        ctr += 1
+print(qctr)
